b1159dc0e6a56a8f64bb40eb39a36126b1680269
Bryan Romero <bryan.haroba@gmail.com>
2025-10-04 17:59:53 -0700

    fix

--- src/modules/mail/mail.module.ts
+++ src/modules/mail/mail.module.ts
@@ -21,7 +21,7 @@ import { MailService } from './mail.service';
           transport: {
             host: mail.host,
             port: mail.port,
-            secure: mail.port === 465, // Cambiar a true si usas el puerto 465
+            secure: false, // Cambiar a true si usas el puerto 465
             socketTimeout: 60000, // 60 segundos
             auth: {
               user: mail.user,
